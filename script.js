92a1df8c1cb92607008eb748c88157c7ae5fc889
Jacques Yonta <jameshd366@gmail.com>
2026-09-15 22:47:30 +0100

    Fix: add type=module to script tag

---
+++ script.js
@@ -0,0 +1,6 @@
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+
+
+// Confirm this bundle started successfully so Capgo can keep it instead of rolling back to the previous bundle.
+CapacitorUpdater.notifyAppReady();
+
